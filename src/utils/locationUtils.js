f053dc00076e382da46edfe0a044cdbd4724368c
Alex Haynes <alex@alexhaynes.org>
2024-10-08 21:53:44 -0400

    fix: use https

--- src/utils/locationUtils.js
+++ src/utils/locationUtils.js
@@ -1,12 +1,10 @@
 export const fetchCityName = async () => {
   try {
-    const response = await fetch('http://ip-api.com/json/');
+    const response = await fetch('https://ipapi.co/json/');
     const data = await response.json();
-    if (data.status === 'success') {
-      return data.city;
-    }
+    return data.city || 'Unknown City';
   } catch (error) {
     console.error('Error fetching location:', error);
+    return 'Unknown City';
   }
-  return null;
 };
